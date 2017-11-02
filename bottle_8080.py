51a93fb584dbf66cfb0e2ab5220627428ceed257
x1001000 <x1001000@gmail.com>
2017-11-03 04:37:32 +0800

    simplify bottle

--- bottle_8080.py
+++ bottle_8080.py
@@ -1,5 +1,4 @@
-from bottle import route, run, template, get, static_file, request
-from time import sleep
+from bottle import route, run
 import control
 
 @route('/<x>')
@@ -25,56 +24,6 @@ def movement_by_pwm(ena,enb):
     
     control.ena.ChangeDutyCycle(abs(ena))
     control.enb.ChangeDutyCycle(abs(enb))
-
-@route('/wonder')
-def wonderwoman():
-    return template('dictate')
-
-@route('/wonder/<sentence>')
-def wonder(sentence):
-    print(sentence)
-    for word in sentence:
-        if word=='跑':
-            control.move('w',1)
-        elif word=='前':
-            control.move('w',0.3)
-        elif word=='進':
-            control.move('w',0.3)
-        elif word=='左':
-            control.move('a',0.3)
-        elif word=='右':
-            control.move('d',0.3)
-        elif word=='後':
-            control.move('s',0.3)
-        elif word=='圈':
-            control.move('j',1)
-            sleep(0.1)
-            control.move('k',1)
-
-@get('/<filename>')
-def js(filename):
-    return static_file(filename, root='static')
-
-@route('/ant')
-def antman():
-    return template('touch')
-
-@route('/ajax', method='POST')
-def ajax():
-    arrow = request.forms.get("arrow")
-
-    stop_key = (87, 83, 65, 68)
-
-    if int(arrow) == 119:
-        control.move('w',0.3)
-    if int(arrow) == 115:
-        control.move('s',0.3)
-    if int(arrow) == 100:
-        control.move('d',0.3)
-    if int(arrow) == 97:
-        control.move('a',0.3)
-    if int(arrow) in stop_key:
-        print('STOP')
 
 try:
     run(host='0.0.0.0', port=8080)
